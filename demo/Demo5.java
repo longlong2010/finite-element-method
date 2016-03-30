64b75d7aea0b78ee8549d9a1ce22b925e487b375
Long Long <longlong2010@gmail.com>
2016-03-30 20:36:50 +0800

    change demo5

--- demo/Demo5.java
+++ demo/Demo5.java
@@ -8,6 +8,8 @@ import visualization.*;
 import model.*;
 import property.*;
 
+//  Center displacement of Simply supported beam
+
 public class Demo5 {
 
 	public static void main(String[] args) {
@@ -27,17 +29,25 @@ public class Demo5 {
 		dimension.setProperty(DimensionProperty.I, I);
 
 		BeamNode n1 = new BeamNode(0, 0);
-		BeamNode n2 = new BeamNode(1, 1);
+		BeamNode n2 = new BeamNode(0.5, 0);
+		BeamNode n3 = new BeamNode(1, 0);
+
 		n1.addConstraint(Constraint.X);
 		n1.addConstraint(Constraint.Y);
-		n1.addConstraint(Constraint.THETA);
+		
+		n3.addConstraint(Constraint.X);
+		n3.addConstraint(Constraint.Y);
 		
 		n2.addLoad(Load.Y, 1);
-		n2.addLoad(Load.X, 1);
 
 		BeamElement e1 = new BeamElement(n1, n2, material, dimension);
+		BeamElement e2 = new BeamElement(n2, n3, material, dimension);
 		s.addElement(e1);
+		s.addElement(e2);
+
 		s.solve();
-		System.out.println(n2.getValue(Dof.THETA));
+		
+		//	1 / (48 * P * L ^3)
+		System.out.println(n2.getValue(Dof.Y));
 	}
 }
